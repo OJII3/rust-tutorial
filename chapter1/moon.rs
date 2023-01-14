cfe91f96d06c2b398ebd967474e3fca41501d527
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-01-14 15:49:27 +0900

    update

--- chapter1/moon.rs
+++ chapter1/moon.rs
@@ -1,3 +1,7 @@
 fn main() {
     let moon = 834400.0;
-    let car_speed = 
+    let car_speed = 80.0;
+    let btrain_speed = 300.0;
+    println!("車で月まで{}", moon / car_speed / 24.0);
+    println!("月まで新幹線で{}", moon/ btrain_speed / 24.0);
+}
